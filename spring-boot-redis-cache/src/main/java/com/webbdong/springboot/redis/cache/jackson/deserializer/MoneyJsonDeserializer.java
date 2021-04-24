ee2b741a5e4ca160960f9bf29e80c1798daf3895
webbdong <wangwebb1991@gmail.com>
2021-04-24 23:09:45 +0800

    MoneyJsonDeserializer

--- spring-boot-redis-cache/src/main/java/com/webbdong/springboot/redis/cache/jackson/deserializer/MoneyJsonDeserializer.java
+++ spring-boot-redis-cache/src/main/java/com/webbdong/springboot/redis/cache/jackson/deserializer/MoneyJsonDeserializer.java
@@ -2,25 +2,24 @@ package com.webbdong.springboot.redis.cache.jackson.deserializer;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.webbdong.springboot.redis.cache.model.vo.MoneyVo;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MoneyJsonDeserializer extends JsonDeserializer<Money> {
 
     @Override
     public Money deserialize(JsonParser p, DeserializationContext ctx) throws IOException, JsonProcessingException {
-        String json = p.getValueAsString();
-        Gson gson = new Gson();
-        Map map = gson.fromJson(json, HashMap.class);
-//        return Money.ofMinor(CurrencyUnit.of((String) map.get("currencyUnitCode")), ((Double) map.get("amount")).longValue());
-        return Money.of(CurrencyUnit.of((String) map.get("currencyUnitCode")), (Double) map.get("amount"));
+        ObjectCodec codec = p.getCodec();
+        JsonNode jsonNode = codec.readTree(p);
+        MoneyVo moneyVo = codec.treeToValue(jsonNode, MoneyVo.class);
+        return Money.of(CurrencyUnit.of(moneyVo.getCurrency()), moneyVo.getAmount());
     }
 
 }
